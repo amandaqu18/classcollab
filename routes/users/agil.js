c00f5337e674a6816a0af56c143f62d535db3d92
amandaqu18 <agil@quinnipiac.edu>
2021-09-26 13:44:59 -0400

    assignment4

--- routes/users/agil.js
+++ routes/users/agil.js
@@ -8,5 +8,10 @@ module.exports = function(router, username) {
       });
   });
 
-  // Set up additional routes here
-};
+  router.get('/' + username + '/aboutme', function(req, res, next) {
+    res.render(username + '/aboutme', {
+      
+      username: username
+   });
+  });
+};[]
